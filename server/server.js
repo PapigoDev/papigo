e7fc07cfe573fed3c55eddb26268a05610d2174c
ziyamustafayev <42975145+ziyamustafayev@users.noreply.github.com>
2023-12-24 14:44:47 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require("dotenv").config()
 
 const app=express()
 app.get("/",(req,res)=>res.send("hello"))
+app.get("/health", (req, res) => res.send("OK"))
 app.use(express.json())
 app.use(cors());
 const PORT=process.env.PORT || 5001
